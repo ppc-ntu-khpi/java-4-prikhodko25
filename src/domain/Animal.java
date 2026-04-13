b2b7eb02aee4861ae9b788cecaa844db82799c11
Приходько Софія <sofiykapiryet@gmail.com>
2026-04-13 15:38:34 +0300

    done

--- src/domain/Animal.java
+++ src/domain/Animal.java
@@ -3,26 +3,26 @@ package domain;
 public class Animal {
 
     protected String name;
-
     protected int weight;
-
+    protected String species;
+    
     public Animal() {
-        name="generic animal";
-        weight=25;
+        name = "Elephant";
+        weight = 1500;
+        species = "Large mammal";
     }
-
     public void eat() {
         System.out.println("Animal eating...");
     }
 
     public void speak() {
-        System.out.println("Animal speaking...");
+        System.out.println("Animal making sounds");
     }
 
     @Override
     public String toString() {
-        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg";
+        return "My animal is " +  name + "\nThe weight this animal is " + weight
+                + "\nand the species this animal is " + species;
     }
 
- 
 }
